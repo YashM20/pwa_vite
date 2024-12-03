6afde9f81ebb1685a72309c8398eebb1c089cf37
Yash Mahajan <67644300+YashM20@users.noreply.github.com>
2024-12-03 10:35:42 +0530

    test2

--- vite.config.ts
+++ vite.config.ts
@@ -7,32 +7,14 @@ export default defineConfig({
     react(),
     VitePWA({ 
       registerType: 'autoUpdate',
-      manifest: {
-        name: 'Your App Name',
-        short_name: 'App',
-        description: 'Your app description',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      },
       devOptions: {
         enabled: true
+      },
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}']
